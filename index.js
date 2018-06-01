ee2014c7a270be6d264d795b060d786ec17a81f1
Luis de Leon <luyzdeleon@gmail.com>
2018-06-01 18:55:04 -0400

    Updated submitTransaction function / added executeQuery function

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var packageData = require('./package.json'),
-    Web3 = require('web3');
+    Web3 = require('web3'),
+    ethAbiDecoder = require('ethereumjs-abi');
 
 function parseOpts(opts) {
   return {
-    ethNode: opts['eth_node'] || 'http://localhost:8545',
+    ethNode: opts['eth_node'] || 'http://127.0.0.1:8545',
     networkId: opts['network_id'] || '5777'
   }
 }
@@ -16,49 +17,77 @@ module.exports.getPluginDefinition = function() {
   }
 }
 
-module.exports.submitTransaction = async function(serializedTx, opts) {
+module.exports.submitTransaction = async function(serializedTx, txMetadata, opts) {
   var parsedOpts = parseOpts(opts),
       ethNode = parsedOpts.ethNode,
       networkId = parsedOpts.networkId,
       web3 = new Web3(new Web3.providers.HttpProvider(ethNode)),
       serializedTx = serializedTx.startsWith('0x') ? serializedTx : ('0x' + serializedTx),
       txHash = null,
-      error = null;
+      error = false,
+      errorMsg = null;
 
   try {
     txHash = await web3.eth.sendRawTransaction(serializedTx);
   } catch (e) {
     console.error(e);
     txHash = null;
-    error = 'Failed to submit transaction';
+    error = true;
+    errorMsg = e;
   }
 
   return {
     hash: txHash,
     metadata: {},
-    error: error
+    error: error,
+    errorMsg: errorMsg
   };
 }
 
-module.exports.verifyTransaction = async function(txHash, opts) {
+module.exports.executeQuery = async function(query, decodeOpts, opts) {
   var parsedOpts = parseOpts(opts),
+      ethNode = parsedOpts.ethNode,
+      networkId = parsedOpts.networkId,
       web3 = new Web3(new Web3.providers.HttpProvider(ethNode)),
-      txReceipt = null,
-      txVerifiable = false,
-      result = false;
+      result = null,
+      decoded = false,
+      error = false,
+      errorMsg = null,
+      rpcResponse = {};
 
-  try {
-    txReceipt = await web3.eth.getTransactionReceipt(txHash);
-    txVerifiable = txReceipt ? true : false;
-    result = txVerifiable && txReceipt.status === '0x1' ? true : false;
-  } catch (e) {
-    console.error(e);
-    txVerifiable = false;
-    result = false;
+  if (typeof query !== 'object') {
+    error = true;
+    errorMsg = 'Invalid query, must be an Object';
+  } else {
+    try {
+      rpcResponse = web3.currentProvider.send({
+        jsonrpc: '2.0',
+        method: query.rpc_method,
+        params: query.rpc_params
+      });
+      result = rpcResponse.result;
+    } catch (e) {
+      console.error(e);
+      result = null;
+      error = true;
+      errorMsg = e;
+    }
+
+    if (query.rpc_method === 'eth_call' && rpcResponse && decodeOpts && Array.isArray(decodeOpts.return_types)) {
+      try {
+        result = ethAbiDecoder.rawDecode(decodeOpts.return_types, [rpcResponse.result]);
+        decoded = true;
+      } catch (e) {
+        console.error(e);
+        decoded = false;
+      }
+    }
   }
 
   return {
-    tx_verifiable: txVerifiable,
-    result: result
+    result: result || null,
+    decoded: decoded,
+    error: error,
+    errorMsg: errorMsg
   };
 }
